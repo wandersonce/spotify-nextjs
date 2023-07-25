224481b5e82eed05850b6ddd7364f38b4735641b
Wanderson Castro <wandersonce1@hotmail.com>
2023-07-25 10:02:09 -0700

    Logic of JWT token

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -1,6 +1,30 @@
-import { LOGIN_URL } from '@/lib/spotify';
 import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
+import spotifyApi, { LOGIN_URL } from '@/lib/spotify';
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setAccessToken(token.refreshToken);
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken();
+    console.log('REFRESHED TOKEN IS:', refreshedToken);
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    };
+  } catch {
+    console.log(error);
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError',
+    };
+  }
+}
 
 const handler = NextAuth({
   providers: [
@@ -10,6 +34,34 @@ const handler = NextAuth({
       authorization: LOGIN_URL,
     }),
   ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      //Initial Sign in
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+        };
+      }
+
+      //Returning the previous token
+      if (Date.now() < token.accessTokenExpires) {
+        console.log('EXISTING TOKEN STILL VALID');
+        return token;
+      }
+
+      //Access token has expired, so refresh it
+      console.log('EXISTING TOKEN HAS EXPIRED');
+      return await refreshAccessToken(token);
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
