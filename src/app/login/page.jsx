fab7d2ac54e5e18704050d50b1933c672e815736
Wanderson Castro <wandersonce1@hotmail.com>
2023-07-25 13:26:16 -0700

    Adjusting Css for login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 function Login() {
   const [providers, setProviders] = useState(null);
   const [providerName, setProviderName] = useState('');
+  const [providerId, setProviderId] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -17,19 +18,25 @@ function Login() {
     if (providers != null) {
       Object.values(providers).map((provider) => {
         setProviderName(provider.name);
+        setProviderId(provider.id);
       });
     }
   }, [providers]);
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center bg-black min-h-screen w-full">
       <img
-        className="w-52 mb-5"
+        className="w-52 mb-8"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/2048px-Spotify_logo_without_text.svg.png"
         alt="Spotify Logo"
       />
       <div>
-        <button>Login with {providerName}</button>
+        <button
+          onClick={() => signIn(providerId, { callbackUrl: '/' })}
+          className="bg-[#18D860] text-white p-5 rounded-lg"
+        >
+          Login with {providerName}
+        </button>
       </div>
     </div>
   );
