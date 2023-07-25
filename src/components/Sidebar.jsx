e370aa685dde53987bf5b354281f333d46744703
Wanderson Castro <wandersonce1@hotmail.com>
2023-07-25 13:36:12 -0700

    Getting Session on Sidebar Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   HomeIcon,
   MagnifyingGlassIcon,
@@ -6,11 +7,21 @@ import {
   RssIcon,
   HeartIcon,
 } from '@heroicons/react/24/outline';
+import { signOut, useSession } from 'next-auth/react';
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Logout</p>
+        </button>
+
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="w-5 h-5" />
           <p>Home</p>
