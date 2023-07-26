3a5ab9199bd1aae579b5aec19c4363545f68e5d0
Wanderson Castro <wandersonce1@hotmail.com>
2023-07-26 10:03:27 -0700

    Adding Lodash and random colors to header

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
+import Center from '@/components/Center';
 import Sidebar from '@/components/Sidebar';
 
 export default function Home() {
   return (
     <>
-      <main className="bg-black h-screen overflow-hidden">
+      <main className="flex bg-black h-screen overflow-hidden">
         {/* Sidebar */}
         <Sidebar />
         {/* Center */}
+        <Center />
       </main>
 
       <div>{/* Player */}</div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ function Sidebar() {
   const { data: session, status } = useSession();
 
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="h-screen text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide">
       <div className="space-y-4">
         <button
           className="flex items-center space-x-2 hover:text-white"
@@ -57,6 +57,11 @@ function Sidebar() {
         <hr className="border-t-[0.1px] border-gray-900" />
 
         {/* Playlist */}
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
         <p className="cursor-pointer hover:text-white">Playlist name...</p>
