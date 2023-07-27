3ac7a883d02a44817cbbe0544276e5d2b5e88750
Wanderson Castro <wandersonce1@hotmail.com>
2023-07-27 10:40:19 -0700

    Starting the bottom player controller

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -30,7 +30,7 @@ function Sidebar() {
     <div
       className="h-screen text-gray-500 p-5 text-xs lg:text-sm border-r 
     border-gray-900 overflow-y-scroll scrollbar-hide sm:max-w-[12rem] lg:max-w-[15rem]
-    hidden md:inline-flex"
+    hidden md:inline-flex pb-36"
     >
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
