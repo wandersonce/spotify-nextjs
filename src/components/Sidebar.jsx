7060d84177195eafdf7c05bc652f12c4532e90c4
Wanderson Castro <wandersonce1@hotmail.com>
2023-07-28 08:47:00 -0700

    Fixing wording

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -57,12 +57,12 @@ function Sidebar() {
 
         <button className="flex items-center space-x-2 hover:text-white">
           <HeartIcon className="w-5 h-5" />
-          <p>Your Library</p>
+          <p>Like Songs</p>
         </button>
 
         <button className="flex items-center space-x-2 hover:text-white">
           <RssIcon className="w-5 h-5" />
-          <p>Like Songs</p>
+          <p>Your Library</p>
         </button>
 
         <hr className="border-t-[0.1px] border-gray-900" />
